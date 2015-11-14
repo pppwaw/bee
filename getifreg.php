3bb9af4fbbf49fc7814b03b4d395c1a11725db03
lj2000lj <lj2000lj@126.com>
2015-11-14 19:09:56 +0800

    使用MySqli的面向对象编程

--- getifreg.php
+++ getifreg.php
@@ -4,10 +4,14 @@ error_reporting(E_ALL ^ E_NOTICE);
 include 'config.php';
 $mysqli=new mysqli($db_host,$db_user,$db_psw,$db_name,$db_port);
 $name=htmlspecialchars($_GET['username']);  
-$sql1= "SELECT * FROM ".$table." WHERE ".$userl."='$name'";
-$rs=mysqli_query($mysqli,$sql1);
-$row=mysqli_affected_rows($mysqli);
-if ($row)
+$sql1= "SELECT * FROM ".$table." WHERE ".$userl."=?"; 
+$stmt1=$mysqli->prepare($sql1);
+$stmt1->bind_param("s",$name);
+$stmt1->execute();
+$result3=$stmt1->get_result();
+$resultdata3=$result3->fetch_all(MYSQLI_ASSOC);
+$count3=count($resultdata3);                                 
+if ($count3>0)
 {
     echo "yes";
     exit ();
