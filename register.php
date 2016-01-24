5c54f2ac286a33baf99595b1f47be9e0e114252f
lj2000lj <lj2000lj@126.com>
2016-01-24 15:48:01 +0800

    修复一些错误

--- register.php
+++ register.php
@@ -10,7 +10,6 @@ function saltgen( $length = 6 ) {
     {
         $password .= $chars[ mt_rand(0, strlen($chars) - 1) ];
     }
-
     return $password;
 }
 $name=htmlspecialchars($_REQUEST['username']);  
@@ -25,7 +24,6 @@ if(mysqli_connect_errno())
     echo mysqli_connect_error();
     exit(0);
 }
-if($usecn=false&&!eregi('^[A-Za-z0-9_]$',$name))die("bee9");
 if(isset($_REQUEST['id']))
 {
     session_id(md5(md5($_REQUEST['id'])));
@@ -34,12 +32,6 @@ if(isset($_REQUEST['id']))
     exit(0);
 }
 session_start();
-if($_SESSION['check'] !=$showing||$showing=="")
-{
-    echo"bee6";
-    session_destroy();
-    exit(0);
-}
 if($name==""|| $password=="")  
 {  
     echo"bee1"; 
@@ -59,50 +51,54 @@ else
     $stmt2=$mysqli->prepare($sql2);
     $stmt2->bind_param("s",$mac);
     $stmt2->execute();
-	$count=count($stmt2->get_result()->fetch_all(MYSQLI_ASSOC));
-        if ($count>$maxreg-1)
-        {
-            echo"bee3";
-            session_destroy();
-            exit(0);    
-        }
-        $sql3="SELECT COUNT(*) AS count FROM ".$table." WHERE ".$regipl."=?";
-        $stmt3=$mysqli->prepare($sql3);
-        $stmt3->bind_param("s",$regip);
-        $stmt3->execute();
-        $count2=count($stmt3->get_result()->fetch_all(MYSQLI_ASSOC));
-        if ($count2>$maxreg-1)
-        {
-            echo"bee3";
-            session_destroy();
-            exit(0);    
-        }
-        $sql1= "SELECT * FROM ".$table." WHERE ".$userl."=?"; 
-        $stmt1=$mysqli->prepare($sql1);
-        $stmt1->bind_param("s",$name);
-        $stmt1->execute();
-        $count3=count($stmt1->get_result()->fetch_all(MYSQLI_ASSOC));                                 
-        if ($count3>0)
-        {                                                             
-            echo "bee0";  
-            session_destroy();
-            exit(0);
-        }
-        $password=md5(md5($password).$salt);
-        $sql="INSERT INTO ".$table." (".$userl.", ".$psdl.", ".$regipl.", ".$saltl.", ".$macl.") VALUES (?,?,?,?,?)";
-        $stmt4 = $mysqli->prepare($sql);
-        $stmt4->bind_param('sssss', $name, $password, $regip,$salt,$mac);
-        if(!$stmt4->execute())
-        {  
-            echo"bee4";
-            session_destroy();
-            exit(0);  
-        }  
-        else   
-        {  
-            echo"ok";
-            session_destroy();
-        }  
-    }  
+    $stmt2->store_result();
+    if ($stmt2->num_rows>$maxreg-1)
+    {
+        echo"bee3";
+        session_destroy();
+        exit(0);    
+    }
+    $stmt2->close();
+    $sql3="SELECT COUNT(*) AS count FROM ".$table." WHERE ".$regipl."=?";
+    $stmt3=$mysqli->prepare($sql3);
+    $stmt3->bind_param("s",$regip);
+    $stmt3->execute();
+    $stmt3->store_result();
+    if ($stmt3->num_rows>$maxreg-1)
+    {
+        echo"bee3";
+        session_destroy();
+        exit(0);    
+    }
+    $stmt3->close();
+    $sql1= "SELECT * FROM ".$table." WHERE ".$userl."=?"; 
+    $stmt1=$mysqli->prepare($sql1);
+    $stmt1->bind_param("s",$name);
+    $stmt1->execute();
+    $stmt1->store_result();
+    if ($stmt1->num_rows>0)
+    {
+        echo "bee0";  
+        session_destroy();
+        exit(0);
+    }
+    $stmt1->close();
+    $password=md5(md5($password).$salt);
+    $sql="INSERT INTO ".$table." (".$userl.", ".$psdl.", ".$regipl.", ".$saltl.", ".$macl.") VALUES (?,?,?,?,?)";
+    $stmt4 = $mysqli->prepare($sql);
+    $stmt4->bind_param('sssss', $name, $password, $regip,$salt,$mac);
+    if(!$stmt4->execute())
+    {
+        echo"bee4";
+        session_destroy();
+        exit(0);  
+    }
+    else   
+    {
+        echo"ok";
+        session_destroy();
+    }
+    $stmt4->close();
+}  
 //核心代码结束
 ?>  
