3f784cb770e98d1574095bdf71c6d7b9cc34467a
lj2000lj <lj2000lj@126.com>
2016-06-16 13:15:04 +0800

    BeeLogin网页端JSON重置版

--- getid.php
+++
@@ -1,4 +0,0 @@
-<?php
-    session_start();
-    echo json_encode(Array("result"=>true,"id"=>session_id()));
-?>
--- getip.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(isset($_REQUEST['n'])){
-    session_id(md5(md5($_REQUEST['n'])));
-    session_start();
-    echo json_encode(Array("ip"=>$_SESSION['ips'],"userName"=>$_REQUEST['n']));
-}
-?>
